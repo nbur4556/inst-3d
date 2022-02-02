fc63113c67ac4ead8e7e0941da3628e0ba5ee097
nbur4556 <nbur4556@gmail.com>
2022-02-02 16:02:41 -0600

    feat: initialize new canvas scene

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert('connected')
+import * as THREE from 'three';
+
+window.onload = () => {
+    const fov = 75;
+    const aspectRatio = window.innerWidth / window.innerHeight
+    const clipping = {
+        near: 0.1,
+        far: 1000
+    }
+
+    // Initialize Scene
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(fov, aspectRatio, clipping.near, clipping.far);
+    const renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    renderer.render(scene, camera);
+}
