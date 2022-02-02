952716467ce9bfe7ab0abf9aceca09977e83c52e
nbur4556 <nbur4556@gmail.com>
2022-02-02 16:52:54 -0600

    feat: set positions of sphere instrument objects

--- src/index.js
+++ src/index.js
@@ -19,17 +19,18 @@ window.onload = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    // Add Objects
+    // Objects
     const sphereInst1 = new SphereInst({ size: 1.5, color: 0xffff00 });
     const sphereInst2 = new SphereInst({ color: 0x00ffff });
+    const sphereInst3 = new SphereInst({ size: 0.75, color: 0xff00ff });
+
+    sphereInst1.setPosition({ x: -2, y: -1 });
+    sphereInst2.setPosition({ x: 2 });
+    sphereInst3.setPosition({ y: 2, z: .95 });
 
     scene.add(sphereInst1.mesh);
     scene.add(sphereInst2.mesh);
-
-    sphereInst1.mesh.position.x = -2;
-    sphereInst1.mesh.position.y = -1;
-    sphereInst2.mesh.position.x = 2;
-    sphereInst2.mesh.position.y = 1;
+    scene.add(sphereInst3.mesh);
 
     camera.position.z = 5;
 
